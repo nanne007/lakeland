7f0cadda02af118837665e45bbd62d3f9595bc3a
Cao Jiafeng <funfriendcjf@gmail.com>
2016-02-28 10:04:08 +0800

    add ex_doc to mix file

--- mix.exs
+++ mix.exs
@@ -35,6 +35,8 @@ defmodule Lakeland.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:earmark, "~> 0.1", only: [:dev]},
+      {:ex_doc, "~> 0.11", only: [:dev]},
       {:dialyxir, "~> 0.3", only: [:dev]}
     ]
   end
